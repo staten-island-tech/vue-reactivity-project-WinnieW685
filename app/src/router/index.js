52a299b5f5b7e339adb44e963b76ba584c617fff
WinnieW685 <wwu82648@gmail.com>
2026-02-10 12:53:35 -0500

    qwer

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/create',
+      path: '/',
       name: 'world',
       component: UserCreate,
     },
